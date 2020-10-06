49f0f763c12b6b26972e2ebac189bf49c3b619a2
Uday Kumar <udaykumaruking@gmail.com>
2020-10-06 21:38:40 +0530

    finished function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -10,9 +12,40 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int temperature = 0;
-  String location = "uday";
+  String location = "London";
+  int woeid = 44418;
+  String weather = 'clear';
 
-  void fetchSearch(String input) {}
+  String searchApiUrl =
+      'https://www.metaweather.com//api/location/search/?query=';
+  String locationApiUrl = 'https://www.metaweather.com//api/location';
+
+  void fetchSearch(String input) async {
+    var searchResult = await http.get(searchApiUrl + input);
+    var result = json.decode(searchResult.body)[0];
+
+    setState(() {
+      location = result["title"];
+      woeid = result["woeid"];
+    });
+  }
+
+  void fetchLocation() async {
+    var locationResult = await http.get(locationApiUrl + woeid.toString());
+    var result = json.decode(locationResult.body);
+    var consolidatedWeather = result["consolidated_weather"];
+    var data = consolidatedWeather[0];
+
+    setState(() {
+      temperature = data["the_temp"].round();
+      weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
+    });
+  }
+
+  void onTextFieldSubmitted(String input) {
+    fetchSearch(input);
+    fetchLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +86,9 @@ class _MyAppState extends State<MyApp> {
                     Container(
                       width: 300,
                       child: TextField(
+                        onSubmitted: (String input) {
+                          onTextFieldSubmitted(input);
+                        },
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 25,
